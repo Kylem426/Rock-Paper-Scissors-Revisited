2c8b1c9ec8dc8d1869c06748a10ba1816a999acb
KyleM426 <105526543+Kylem426@users.noreply.github.com>
2023-06-23 15:19:30 -0500

    Final Update
    I got it working to satisfaction.

--- Rock-Paper-Scissors.js
+++ Rock-Paper-Scissors.js
@@ -1,3 +1,17 @@
+const btnRock = document.getElementById('rock');
+const btnPaper = document.getElementById('paper');
+const btnScissors = document.getElementById('scissors');
+const btnClear = document.getElementById('clear');
+const list = document.getElementById('list');
+const score = document.getElementById('score');
+const winner = document.getElementById('winner');
+
+let playerName = prompt("Who is the challenger?");
+
+let playerScore = 0;
+let computerScore = 0;
+score.textContent = `${playerName} = ${playerScore} Computer = ${computerScore}`
+
 function getComputerChoice() {
     let random = Math.random();
     if(random <= 0.33) {
@@ -9,42 +23,69 @@ function getComputerChoice() {
     }
 }
 
-let computerChoice = getComputerChoice();
+btnRock.addEventListener("click", function() {
+    playRound("rock", getComputerChoice());
+}); 
 
-let playerSelection = prompt("Rock, Paper, or Scissors?");
+btnPaper.addEventListener("click", function(){
+    playRound("paper", getComputerChoice());
+});
 
-let playerChoice = playerSelection.toLowerCase();
+btnScissors.addEventListener("click", function(){
+    playRound("scissors", getComputerChoice());
+});
+
+function clear() {
+    while(list.firstChild) {
+        list.removeChild(list.firstChild);
+    }
+    score.textContent = `${playerName} = ${playerScore = 0} Computer = ${computerScore = 0}`;
+    winner.textContent = " ";
+}
+
+btnClear.addEventListener("click", clear);
+
+function youWin() {
+    const listItem = document.createElement('li');
+    listItem.textContent = "You win!";
+    list.appendChild(listItem);
+}
+
+function youLose() {
+    const listItem = document.createElement('li');
+    listItem.textContent = "You lose!";
+    list.appendChild(listItem);
+}
+
+function aDraw() {
+    const listItem = document.createElement('li');
+    listItem.textContent = "It's a draw!";
+    list.appendChild(listItem);
+}
 
 function playRound(playerChoice, computerChoice) {
-    if(playerChoice === "rock" && computerChoice === "rock") {
-        return "It's a draw!";
-    }else if(playerChoice === "rock" && computerChoice === "paper") {
-        return "You lost this round!";
-    }else if (playerChoice === "rock" && computerChoice === "scissors") {
-        return "You won this round!";
-    }else if (playerChoice === "paper" && computerChoice === "rock") {
-        return "You won this round!";
-    }else if (playerChoice === "paper" && computerChoice === "paper") {
-        return "It's a draw!";
-    }else if (playerChoice === "paper" && computerChoice === "scissors") {
-        return "You lost this round!";
-    }else if (playerChoice === "scissors" && computerChoice === "rock") {
-        return "You lost this round!";
-    }else if (playerChoice === "scissors" && computerChoice === "scissors") {
-        return "It's a draw!";
-    }else  if(playerChoice ==="scissors" && computerChoice === "paper") {
-        return "You won this round!";
+    if((playerChoice === "rock" && computerChoice === "rock") || (playerChoice === "paper" && computerChoice === "paper") 
+    || (playerChoice === "scissors" && computerChoice === "scissors") ) {
+        aDraw();
+    }else if((playerChoice === "rock" && computerChoice === "paper") || (playerChoice === "paper" && computerChoice === "scissors") 
+    || (playerChoice === "scissors" && computerChoice === "rock")) {
+        youLose();
+        score.textContent = `${playerName} = ${playerScore} Computer = ${computerScore += 1}`;
+        checkScore(playerScore, computerScore);
+    }else if ((playerChoice === "rock" && computerChoice === "scissors") || (playerChoice === "paper" && computerChoice === "rock")
+    || (playerChoice === "scissors" && computerChoice === "paper")) {
+        youWin();
+        score.textContent = `${playerName} = ${playerScore += 1} Computer = ${computerScore}`;
+        checkScore(playerScore, computerScore);
     }else {
         return "error";
     }
 }
 
-function game() {
-    computerChoice = getComputerChoice();
-    playerSelection = prompt("Rock, Paper, or Scissors?")
-    playerChoice = playerSelection.toLowerCase();
-    for(let i = 0; i < 5; i++) {
-       result = playRound(playerChoice, computerChoice);
-       return result;
+function checkScore(playerScore, computerScore) {
+    if(playerScore === 5) {
+        winner.textContent = `${playerName} won the game ${playerScore} points to ${computerScore}!`;
+    }else if(computerScore === 5) {
+        winner.textContent = `${playerName} lost the game ${playerScore} points to ${computerScore}!`;
     }
 }
